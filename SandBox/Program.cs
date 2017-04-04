1e5de0da6972b0b68c1e43280804e368ecd719c7
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2017-04-04 23:04:23 +0200

    added sandbox and TODOs

---
+++ SandBox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static (int x, int y) Point => (42, 43);
+    }
+}
